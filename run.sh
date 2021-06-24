c28cf259394aa4e2fb8a035cf9243cdbe2c72274
gntxy <86417999+gntxy@users.noreply.github.com>
2021-06-24 14:13:54 +0000

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,32 @@
+# JANGAN DI RECODE YA ANAK KONTOL
+# RECODE MANDUL 900 TURUNAN
+# YG RECODE BESOK MATI
+# YG RECODE GUA SUMPAHIN SIAL 901891010 TURUNAN
+echo 'KALO ADA PILIHAN GDM3 ATAU LIGHT GDM PILIH LIGHTGDM'
+echo 'sleep sebentar untuk baca tuh diatas sat'
+sleep 30
+apt-get update -y && 
+apt-get upgrade -y &&
+apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev -y &&
+apt-get install liblzma-dev -y && 
+apt install lzma -y && 
+sudo apt install python3 -y &&
+apt install firefox -y &&
+apt install nano -y &&
+apt install wmctrl -y &&
+apt install pip -y &&
+pip install telethon -y &&
+pip install async_generator -y &&
+pip install colorama -y &&
+pip install requests -y &&
+pip install asyncio -y &&
+pip install pytz -y &&
+pip install bs4 -y &&
+sudo apt update -y &&
+sudo apt upgrade -y &&
+sudo apt install xfce4 xfce4-goodies xorg dbus-x11 x11-xserver-utils -y &&
+sudo apt install tigervnc-standalone-server tigervnc-common -y &&
+vncserver 
+
+echo 'selesai ngab install vps nya'
+echo 'watermark @gntcyy '
